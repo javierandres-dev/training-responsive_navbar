7839576931270e3780f161e9b30463b401a497c6
Javier Andrés <55107141+JA-programmer@users.noreply.github.com>
2021-04-05 23:29:12 -0500

    Training

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,18 @@
-'use stric';
-console.log('... working ...');
+'use strict';
+const d = document,
+  $btnShow = d.getElementById('btnShow');
+
+d.addEventListener('DOMContentLoaded', () => {
+  eventListeners();
+});
+
+const eventListeners = () => {
+  $btnShow.addEventListener('click', handleShow);
+};
+
+const handleShow = () => {
+  const $navbar = d.getElementById('navbar');
+  const $social = d.getElementById('social');
+  $navbar.classList.toggle('show');
+  $social.classList.toggle('show');
+};
